85ef23d55dffb5e902c529f2e740a6c19e8ac6f8
Shokin <saindouhamza@gmail.com>
2024-09-12 11:14:42 +0200

    Étape 1.2 : Réalisation du filtre des travaux : Ajout des filtres pour afficher les travaux par catégorie

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -2,26 +2,41 @@
 const projet =  reponse.json();
 console.log(projet)*/
 
+let works = [];
+
+function displayAll(projects) {
+    let newElements = '';
+    for (let i = 0; i < projects.length; i++) {
+        newElements += '<figure>' +
+            '<img src=\"' + projects[i].imageUrl + '\" alt=\"' + projects[i].title + '\">' +
+            '<figcaption>' + projects[i].title + '</figcaption>' +
+        '</figure>';
+    }
+    
+    document.getElementById('gallery').innerHTML = newElements;
+}
+
 const start = async function() {
     const response = await fetch(`http://localhost:5678/api/works`);
     response.json().then(data => {
-        //console.log(value)
-        /*<figure>
-			<img src="assets/images/abajour-tahina.png" alt="Abajour Tahina">
-			<figcaption>Abajour Tahina</figcaption>
-		</figure>*/
-
-        let newElements = '';
+        works = data;
         
-        for (let i = 0; i < data.length; i++) {
+        displayAll(data);
+    })
+}
+
+function filter(idCategory){
+    let newElements = '';
+
+    for ( let i = 0; i < works.length; i++) {
+        if ( works[i].category.id === idCategory) {
             newElements += '<figure>' +
-                '<img src=\"' + data[i].imageUrl + '\" alt=\"' + data[i].title + '\">' +
-                '<figcaption>' + data[i].title + '</figcaption>' +
+                '<img src=\"' + works[i].imageUrl + '\" alt=\"' + works[i].title + '\">' +
+                '<figcaption>' + works[i].title + '</figcaption>' +
             '</figure>';
         }
-
-        document.getElementById('gallery').innerHTML = newElements;
-    })
+    }
+    document.getElementById('gallery').innerHTML = newElements;
 }
 
 start()
